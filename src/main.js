94aa734bab0d3ff6cf2ab35383ec4c880907162f
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-03-22 19:13:51 -0700

    add json packages

--- src/main.js
+++ src/main.js
@@ -7,18 +7,18 @@ class Temp {
 
 var Temp = () => {
     //number “212” immediately prints inside of the span with the ID "boiling-temp" using an arrow function
-    document.getElementById('boiling-temp').innerHTML = 212;
+    document.getElementById('boiling-temp').innerHTML += 212;
     //number “32” immediately prints inside of the span with the ID "freezing-temp" using an arrow function
-    document.getElementById('freezing-temp').innerHTML = 32;
+    document.getElementById('freezing-temp').innerHTML += 32;
     //"88" immediately prints inside of the span with the ID "water-temp" using an arrow function
-    document.getElementById('water-temp').innerHTML = 73;
+    document.getElementById('water-temp').innerHTML += 73;
 }; 
 
 //timeout 
 
-setTimeout( function() {
+setTimeout(function() {
     document.getElementById('boiling').classList.add('show');
     document.getElementById('boiling').classList.remove('hide');
-}, 3000)
+}, 3000);
 
 
